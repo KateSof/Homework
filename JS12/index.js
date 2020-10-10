ee1c4f89aca7a6370fce0b38da635a45737ca251
KateSof <69856602+KateSof@users.noreply.github.com>
2020-10-10 18:31:34 +0300

    Add files via upload

--- JS12/index.js
+++ JS12/index.js
@@ -1,9 +1,28 @@
-const imgs = document.querySelectorAll(".image-to-show")
-const btn = document.getElementById('btn')
+let showImage;
 
-btn.addEventListener('click',pushButton)
-function pushButton(){
-    for (i = 0; i < imgs.length; i++) {
-        imgs[i].className = imgs[i].className.toggle('active');
-      }
+function showNextImage() {
+    showImage = setInterval(
+        function(){
+    let currentImg = document.querySelector(".image-to-show");
+    currentImg.className = "img image-to-hide";
+    const newImage = currentImg.nextElementSibling;
+    if (newImage) {
+        newImage.className = "img image-to-show";
+    } else {
+        const firstImage = document.querySelector(".img");
+        firstImage.className = "img image-to-show";
+    }
+}, 1000)
 }
+showNextImage()
+// showImage = setInterval(showNextImage,1000)
+const stop = document.querySelector('#stop')
+const run = document.querySelector('#run')
+
+function stopImage(){
+    clearInterval(showImage);
+}
+stop.addEventListener('click', stopImage)
+run.addEventListener('click', countinueContinuing)
+
+function countinueContinuing(){     showNextImage() }
